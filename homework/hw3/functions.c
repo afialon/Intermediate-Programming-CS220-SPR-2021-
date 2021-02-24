04ab04f7e5b800d5be36ed8683d90ad06187347a
Alex Fialon <afialon1@ugradx.cs.jhu.edu>
2021-02-23 22:51:30 -0500

    Started Program

---
+++ homework/hw3/functions.c
@@ -0,0 +1,18 @@
+#include "functions.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int read_file(FILE *fp, char words[][MAX_WORD_SIZE + 1], int size) {
+    // TODO: Implement Me!
+}
+
+int match(const char *regex, const char *word, int restriction) {
+  //
+  //
+  //
+  //
+  //
+  
+}
+
