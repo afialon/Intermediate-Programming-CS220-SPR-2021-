99649e63089630e4fbaa4699feba59f38800880d
Alex Fialon <afialon1@ugradx.cs.jhu.edu>
2021-02-25 18:11:07 -0500

    Preliminarily finished the scanner, started on everything else

--- homework/hw3/functions.c
+++ homework/hw3/functions.c
@@ -1,10 +1,26 @@
 #include "functions.h"
-
 #include <stdio.h>
 #include <string.h>
 
 int read_file(FILE *fp, char words[][MAX_WORD_SIZE + 1], int size) {
-    // TODO: Implement Me!
+  //Skip the line with the size
+
+  //Set words length to the size
+  words = words[size][MAX_WORD_SIZE + 1];
+  int endOf = getc(fp);
+  //Go to every line in the file
+  for(int i = 0; i < size; i++) {
+    //
+    //Might need to add a catch for blank words
+    do{
+      //
+      char[] curWord = 
+      words[i] = curWord;
+      endOf = getc(fp);
+    }
+    while (endOf != EOF)
+    
+  
 }
 
 int match(const char *regex, const char *word, int restriction) {
