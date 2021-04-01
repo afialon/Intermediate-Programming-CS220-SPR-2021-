38a71f9dac5cbc223ca1caced301284c3ebd31f3
Alex Fialon <afialon1@ugradx.cs.jhu.edu>
2021-04-01 19:06:49 -0400

    Started main switch statement

---
+++ homework/hw5/phone_db.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <map>
+#include <cctype>
+
+using std::vector;
+using std::string;
+using std::map;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+  //
+  cout << "Info: Welcome to the Phone Database!" << endl;
+  cout << "Info: Please enter a command" << endl;
+  char cmnd;
+  cin >> cmnd;
+  while(cmnd != 'Q') {
+    switch(cmnd) {
+      case 'C':
+
+	break;
+      case 'L':
+
+        break;
+      case 'N':
+
+        break;
+      case 'P':
+
+        break;
+      case 'D':
+
+        break;
+      case 'X':
+
+        break;
+      case 'S':
+
+        break;
+
+    
+
+
+
+    }
+
+  }
+
+  return 0;
+}
