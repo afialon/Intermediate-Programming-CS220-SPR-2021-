8362eda5a1224e61d3835507189f38883a92482c
Alex Fialon <afialon1@ugradx.cs.jhu.edu>
2021-04-03 19:50:12 -0400

    Created the structs and started coding them

--- homework/hw5/phone_db.cpp
+++ homework/hw5/phone_db.cpp
@@ -12,7 +12,19 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+struct phoneNumberColl {
+  string pn_Type;
+  string pn_Number;
+}
+
+struct Name {
+  string cnt_FName;
+  string cnt_LName;  
+}
+
+      
 int main() {
+  map<Name, PhoneNumberColl> phone_db
   //
   cout << "Info: Welcome to the Phone Database!" << endl;
   cout << "Info: Please enter a command" << endl;
@@ -21,25 +33,27 @@ int main() {
   while(cmnd != 'Q') {
     switch(cmnd) {
       case 'C':
-
+	string cFName;
+	string cLName;
+	cin >> 
 	break;
       case 'L':
-
+	
         break;
       case 'N':
-
+	
         break;
       case 'P':
-
+	
         break;
       case 'D':
-
+	
         break;
       case 'X':
-
+	
         break;
       case 'S':
-
+	
         break;
 
     
