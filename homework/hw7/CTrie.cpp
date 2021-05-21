1656911f370da3a953be22e0731e5c345dd3b063
Alex Fialon <afialon1@ugradx.cs.jhu.edu>
2021-05-21 18:36:01 -0400

    Fixed ^ and += operators, changed the destructor, deleted createword and reimplemented its functions in a method, testing

--- homework/hw7/CTrie.cpp
+++ homework/hw7/CTrie.cpp
@@ -5,125 +5,168 @@
 #include <vector>
 #include <iterator>
 // TODO: add other #includes as needed
-
 #include "CTrie.h"
+#include <algorithm>
+
+using std::map;
 
 // TODO: implement the CTrie member functions and friend functions
-CTrie& operator=(const CTrie &rhs) {
+CTrie& CTrie::operator=(const CTrie &rhs) {
    for(map<char, CTrie*>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
      delete (it-> second);
    }
-   map.clear();
+   edgeMap.clear();
    //TODO: Ask TA about correct form
-   CTrie copiedCTire = new CTrie(rhs*);
-   //this.isWord = (rhs->isWord);
-   //this.letter = (rhs->letter);
-   //this.edgeMap =
+   isWord = rhs.isWord;
+   letter = rhs.letter;
+   for(map<char, CTrie*>::const_iterator it = (rhs.edgeMap).cbegin(); it != (rhs.edgeMap).cend(); ++it) {
+     char key = (it->first);
+     CTrie* value = (it->second);
+     edgeMap[key] = value;
+   }
 
-   this = copiedCTrie;
-
+   return *this;
 
 }
 
- CTrie& operator+=(const std::string& word) {
-   char cLetter = word[0];
-   if(word.size() == 0) {
-     this-> isWord = true;
-   }
-   else if(edgeMap.find(cLetter) == edgeMap.end()) {
-     edgeMap[cLetter] = CTrie(word);
-   }
-   else {
-     this +=(word + 1);
-   }
-   return this;
-}
-
-bool operator^(const std::string &word) const {
-  char cLetter = word[0];
-  if(word.size() == 0) {
-    return true;
+CTrie& CTrie::operator+=(const std::string& word) {
+  const char c = word[0];
+  const CTrie* child = getChild(c);
+    
+  if (!child) {
+    CTrie* n = new CTrie(c);
+    edgeMap[c] = n;
   }
-  else if(edgeMap.find(cLetter) == edgeMap.end()) {
+
+  if(word.size() > 1) {
+    *edgeMap[c] += word.substr(1);
+  }
+  return *this;
+}
+
+bool CTrie::operator^(const std::string &word) const {
+  const CTrie* child = getChild(word[0]);
+  if (!child) {
     return false;
   }
   else {
-    return this ^(word + 1);
-  }
-}
-
-bool operator==(const CTrie& rhs) const {
-  if(edgeMap.hasChild() && rhs.hasChild()) {
-    for(map<char, CTrie>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
-      if(rhs[(it->first] == rhs.end() {
-	 return false;
-      }
-	  //can I call straight up size or do I have to specify
-      else if(rhs.size() != edgeMap.size()) {
-	return false;
-      }
-      else {
-	return (it->second)==(rhs->second);
-      }
+    if (word.size() == 1) {
+      return true;
+    }
+    else {
+      return *child ^ word.substr(1);
     }
   }
-  else if(!(edgeMap.hasChild()) && !(rhs.hasChild())) {
-    delete this;
-    //how can I make this function end?
-    return true;
+}
+
+bool CTrie::operator==(const CTrie& rhs) const {
+  CTrie copiedCTrie = CTrie(rhs);
+  
+  for(map<char, CTrie*>::const_iterator it = edgeMap.cbegin(); it != edgeMap.cend(); ++it) {
+    if(!rhs.hasChild(it->first)) {
+      return false;//janky?
+    }
+    else {
+      return *(it->second)== *rhs.getChild(it->first);
+    }
+    
   }
-  else {
-    return false;
-  }
+  return true;
       //iterate through the map, compare every node in this map,
       //also looking for extra codes
     //run a quick test to make sure size is equal
-} 
-
-friend std::ostream& operator<<(std::ostream& os, const CTrie& ct) {
-  string s = "";
-  output_trie(os, s, ct);
-
 }
 
-string output_trie(std::ostream& os, std::string s, const CTrie& ct){
+void output_trie(std::string s,  std::vector<std::string>& v, const CTrie& ct){
   if(ct.isEndpoint()) {
-    std::cout << s;
-    return s;
+    v.push_back(s);
   }
-  for(map<char, CTrie>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
-    s.push_back((this->first));
-    output_trie(os, s, (this->second));
+  if(!(ct.hasChild())) {
+    return;
+  }
+  char c = 97;
+  while(c <= 122) {
+    if(ct.hasChild(c)) {
+      s.push_back(c);
+      output_trie(s, v, *ct.getChild(c));
+    }
+    c++;
   }
 }
 
-unsigned numChildren() const {
-    return edgeMap.size();
+
+std::ostream& operator<<(std::ostream& os, const CTrie& ct) {
+  std::string s;
+  std::vector<std::string> returnString;
+  output_trie(s, returnString, ct); //would this update returnstring
+  std::sort(returnString.begin(), returnString.end());
+
+  //for(std::vector<std::string>::iterator it = returnString.begin(); it != returnString.end(); ++it) {
+  for(std::string s : returnString) {
+    os << s << " ";
+  }
+  os << std::endl;
+  return os;
+}
+
+unsigned int CTrie::numChildren() const {
+  return edgeMap.size();
 }
  
-bool hasChild() const {
+bool CTrie::hasChild() const {
   if(edgeMap.empty()) {
     return false;
   }
   return true;
 }
  
-bool hasChild(char character) const {
+bool CTrie::hasChild(char character) const {
   if(edgeMap.find(character) != edgeMap.end()) {
     return true;
   }
   return false;
 }
 
-const CTrie* getChild(char character) const {
-  if(edgeMap.find(character) != edgeMap.end()) {
-    return edgeMap[character];
+const CTrie* CTrie::getChild(char character) const {
+  std::map<char, CTrie*>::const_iterator temp = edgeMap.find(character);
+  if (temp != edgeMap.end()) {
+    return (temp->second);
   }
+  //if(edgeMap.find(character) != edgeMap.end()) {
+  //const CTrie* temp = edgeMap[character];
+  //return temp;
+  //}
   else {
     return nullptr;
   }
 }
 
-bool isEndpoint() const {
+bool CTrie::isEndpoint() const {
   return isWord;
-};
+}
+
+CTrie* CTrie::createWord (const std::string word) const {
+  std::cout << "creating word for word: " << word << std::endl;
+    if(word.empty()) {
+      CTrie cCTrie = CTrie();
+      CTrie* nCTrie = &cCTrie;
+      return nCTrie;
+    }
+    else if(word.length() == 1) {
+      CTrie cCTrie = CTrie(word[0], true);
+      CTrie* nCTrie = &cCTrie;
+      return nCTrie;
+    }
+    else {
+      //Create a new node for the first letter
+      char cLetter = word[0];
+      CTrie cCTrie = CTrie(cLetter);
+      //is this a natural construction
+      //initially word + 1
+      CTrie* nCTrie = createWord(word.substr(1));
+      cCTrie.edgeMap[word[1]] = nCTrie;
+      //Recursively call for the rest of the word
+      CTrie* pCTrie = &cCTrie;
+      return pCTrie;
+    }
+  }
--- homework/hw7/CTrie.h
+++ homework/hw7/CTrie.h
@@ -4,7 +4,10 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <iostream> //DELETE THIS LATER DONT FORGET DINGUS
+#include <exception>
 
+using std::string;
 
 class CTrie {
 public:
@@ -12,14 +15,26 @@ public:
    * Default Constructor.
    */
   //What do i put in here for map
-  CTrie(): isWord(false), char(''), edgeMap(std::map<char, CTrie*>) { } ;
+  CTrie(): isWord(false), letter('-') { edgeMap = std::map<char, CTrie*>(); }
 
   /**
    * Copy Constructor.
    * \param rhs A const reference to the input to copy.
    */
   //TODO: Create Deep Copy
-  CTrie(const CTrie& rhs): isWord(rhs.isWord), letter(rhs.letter), edgeMap(rhs.edgeMap) { };
+  CTrie(const CTrie& rhs): isWord(false), letter(' ')  {
+    edgeMap = std::map<char, CTrie*>();
+    *this = rhs;
+  }
+
+  CTrie(const char character): isWord(false), letter(character) { edgeMap = std::map<char, CTrie*>(); }
+
+  CTrie(const char character, const bool boolean): isWord(boolean), letter(character) { edgeMap = std::map<char, CTrie*>(); }
+
+  //CTrie(string& word) {
+    
+    //isWord}
+  
   
   /**
    * Destructor.
@@ -27,68 +42,32 @@ public:
   //recursively deletes all of objects children then o
   //iterates through the map, deletes all children, deletes map, then other parameters
   ~CTrie() {
-    for(map<char, CTrie*>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
+
+    for(std::map<char, CTrie*>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
       delete (it-> second);
     }
-    map.clear();
-    delete this;
-  };
+
+    edgeMap.clear();
+   };
 
   /**
    * Assignment operator.
    */
-  CTrie& operator=(const CTrie &rhs) {
-    for(map<char, CTrie*>::iterator it = edgeMap.begin(); it != edgeMap.end(); ++it) {
-      delete (it-> second);
-    }
-    map.clear();
-    //TODO: Ask TA about correct form
-    CTrie copiedCTire = new CTrie(rhs*);
-    //this.isWord = (rhs->isWord);
-    //this.letter = (rhs->letter);
-    //this.edgeMap =
-
-    this = copiedCTrie;
-    
-
-  };
+  CTrie& operator=(const CTrie &rhs);
 
   /**
    * Overloaded addition/assignment operator to input a new string into the Trie.
    * \param word is the word to input into the operator.
    * \return a reference to the CTrie object
    */
-  CTrie& operator+=(const std::string& word) {
-    char cLetter = word[0];
-    if(word.size() == 0) {
-      this-> isWord = true;
-    }
-    else if(edgeMap.find(cLetter) == edgeMap.end()) {
-      edgeMap[cLetter] = CTrie(word);
-    }
-    else {
-      this +=(word + 1);
-    }
-    return this;
-  };
+  CTrie& operator+=(const std::string& word);
 
   /**
    * Overloaded carat operator to check whether a given word exists in the object.
    * \param word the word to find.
    * \return true if the word is a member of the trie, false otherwise
    */
-  bool operator^(const std::string &word) const {
-    char cLetter = word[0];
-    if(word.size() == 0) {
-      return true;
-    }
-    else if(edgeMap.find(cLetter) == edgeMap.end()) {
-      return false;
-    }
-    else {
-      return this ^(word + 1);
-    }
-  };
+  bool operator^(const std::string &word) const;
 
   /**
    * Overloaded equality operator to check whether two Trie objects are equal.
@@ -96,13 +75,7 @@ public:
    * \return true if the other object represents exactly the same set of words,
    *         false otherwise
    */
-  bool operator==(const CTrie& rhs) const {
-    for(map<char, CTrie>::iterator it = phone_db.begin(); it != phone_db.end(); ++it) {
-      //iterate through the map, compare every node in this map, also looking for extra nodes
-      //run a quick test to make sure size is equal
-      //
-    }
-  };
+  bool operator==(const CTrie& rhs) const;
 
   /**
    * \brief Overloaded output stream operator<< to print the CTrie in 
@@ -113,27 +86,17 @@ public:
    * \param q A constant reference to the input CTrie object
    * \return A reference to the output stream object
   */
-  friend std::ostream& operator<<(std::ostream& os, const CTrie& ct) {
-    
-
-  };
+  friend std::ostream& operator<<(std::ostream& os, const CTrie& ct);
 
   /**
    * \return the number of children
    */
-  unsigned numChildren() const {
-    return edgeMap.size();
-  };
+  unsigned numChildren() const;
 
   /**
    * \return true if there are any children, false otherwise
    */
-  bool hasChild() const {
-    if(edgeMap.empty()) {
-      return false;
-    }
-    return true;
-  };
+  bool hasChild() const;
 
   /**
    * Check whether a child linked by a specific character exists.
@@ -141,12 +104,7 @@ public:
    * \return true if there is a link to a child labeled with the character,
    *         false otherwise
    */
-  bool hasChild(char character) const {
-    if(edgeMap.find(character) != edgeMap.end()) {
-      return true;
-    }
-    return false;
-  };
+  bool hasChild(char character) const;
 
   /**
    * Get pointer to child node reachable via link labeled with
@@ -154,58 +112,29 @@ public:
    * \param character a character
    * \return pointer to child node, or nullptr if there is no such child
    */
-  const CTrie* getChild(char character) const {
-    if(edgeMap.find(character) != edgeMap.end()) {
-      return nullptr;
-    }
-    else {
-      return edgeMap[character];
-    }
-  };
+  const CTrie* getChild(char character) const;
+
 
   /**
    * \return true if this node is an endpoint, false otherwise
    */
-  bool isEndpoint() const {
-    return isWord;
-  }; 
+  bool isEndpoint() const; 
+
+  /**
+   *
+   *
+   *
+   */
+  CTrie* createWord (const string word) const;
 
 private:
   // TODO: fields
   //is true if the word is complete, and is false if not
   bool isWord;
-  const char letter;
+  char letter;
   //This map contains up to 26 values, each of which is represented by an actual letter and the edge to the node representing that SPECIFIC letter
   std::map<char, CTrie*> edgeMap;
   
-  
-  // TODO: helper functions
-  /**
-  * \return true if this node is an endpoint, false otherwise
-  */
-  //is the edgemap call correct
-  CTrie(const char character): isWord(false), char(character), edgeMap(std::map<char, CTrie*> edgeMap) { } ;
-
-  CTrie(const char character, const bool boolean): isWord(boolean), char(character), edgeMap(std::map<char, CTrie*> edgeMap) { } ;
-  
-  CTrie(const string& word):  {
-    if(word.empty()) {
-      return null;
-    }
-    else if((word.size() / std::string.size()) == 1) {
-      return CTrie(word[0], true);
-    }
-    else {
-      //Create a new node for the first letter
-      char cLetter = word[0];
-      CTrie cCTrie = CTrie(cLetter);
-      //is this a natural construction
-      cCTrie.edgeMap[word[1]] = CTrie(word + 1)*;
-      //Recursively call for the rest of the word
-      return CTrie;
-    }
-     
-  };
 };
 
 #endif // CTRIE_H
